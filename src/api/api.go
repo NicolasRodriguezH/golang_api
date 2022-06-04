4644ccec3824c1032c695ef45e53f4e0e7393881
Nicolas <nicolashilarion420@gmail.com>
2022-06-04 12:10:17 -0500

    GO API REST IS RUNNING-TESTS WITH POSTMAN

--- src/api/api.go
+++ src/api/api.go
@@ -114,4 +114,43 @@ func DeleteTodo(w http.ResponseWriter, req *http.Request) {
 	w.Write(json)
 }
 
-func UpdateTodo() {}
+func UpdateTodo(w http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+	id := vars["id"]
+
+	bodyTodo, success := helpers.DecodeBody(req)
+	if success != true {
+		http.Error(w, "could not decodde body", http.StatusBadRequest)
+		return
+	}
+
+	var data Data = Data{Errors: make([]string, 0)}
+	bodyTodo.Description = strings.TrimSpace(bodyTodo.Description)
+	if !helpers.IsValidDescription(bodyTodo.Description) {
+		data.Success = false
+		data.Errors = append(data.Errors, "invalid description")
+
+		json, _ := json.Marshal(data)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(json)
+
+		return
+	}
+
+	todo, success := models.Update(id, bodyTodo.Description)
+	if success != true {
+		data.Errors = append(data.Errors, "could not update todo")
+	}
+
+	data.Success = true
+	data.Data = append(data.Data, todo)
+
+	json, _ := json.Marshal(data)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(json)
+	return
+}
